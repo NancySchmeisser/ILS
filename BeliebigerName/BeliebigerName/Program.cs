da0945a032661b35649d6ee6b0bee4bcd9c5fb23
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-10-23 11:37:36 +0200

    Erste Übung

---
+++ BeliebigerName/BeliebigerName/Program.cs
@@ -0,0 +1,21 @@
+﻿/* #################################
+Einsendeaufgabe 1.1
+#################################*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Cshp01cAufgabe
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Dieses Programm ist nun fehlerfrei");
+		}
+	}
+}
+                                                                                                                                                                                                                                                                                                           
---
+++ Hallo/Hallo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hallo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Welt!");
+            Console.WriteLine("Grüßli!");
+        }
+    }
+}
