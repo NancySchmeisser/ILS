a4c7eec4d01fa698e66f8352ff57cc7752f4e806
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-10-27 10:02:31 +0100

    verkürzter Quellcode 3.1

--- CSHP02D/CSHP02D/Program.cs
+++ CSHP02D/CSHP02D/Program.cs
@@ -7,36 +7,22 @@ namespace CSHP02D
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.Write("5 + 9\t\t= ");
-            Console.WriteLine(5 + 9);
-
-            Console.Write("5 - 9\t\t= ");
-            Console.WriteLine(5 - 9);
-
-            Console.Write("5 * 9\t\t= ");
-            Console.WriteLine(5 * 9);
-
-            Console.Write("9 / 5\t\t= ");
-            Console.WriteLine(9 / 5);
-
-            Console.Write(" Rest ");
-            Console.WriteLine( 9 % 5);
-
-            Console.Write("10 + 8 /2\t\t= ");
-            Console.WriteLine(10 + 8 / 2);
-
-            //Geänderte Reihenfolge durch Klammern
-
-            Console.Write("(10 + 8) / 2\t\t= ");
-            Console.WriteLine((10 + 8) / 2);
+        {  
 
             Console.WriteLine("5 + 9\t\t= {0}", 5 + 9);
+            Console.WriteLine("5 - 9\t\t= {0}", 5 -9);
+            Console.WriteLine("5 * 9\t\t= {0}", 5 * 9);
 
+            // Ausgabe mit 2 Platzhaltern
 
+            Console.WriteLine("9 / 5\t\t= {0} Rest {1}", 9 / 5, 9 % 5);
+            Console.WriteLine("10 + 8 /2\t\t= {0}", 10 + 8 / 2);
+            
+            // Geänderte Reihenfolge durch Klammern
 
-
-
+            Console.WriteLine("(10 + 8) / 2\t\t= {0}", (10 + 8) / 2);
+            
+            
         }
     }
 }
