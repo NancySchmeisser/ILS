1d20edbc6087a4bd9d9bc9442a810c1ac6b05a32
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-10-28 11:25:56 +0100

    Ringtausch 3.4

--- CSHP02D 3.2 Variablen/CSHP02D 3.2 Variablen/Program.cs
+++ CSHP02D 3.2 Variablen/CSHP02D 3.2 Variablen/Program.cs
@@ -8,17 +8,36 @@ namespace CSHP02D_3._2_Variablen
     {
         static void Main(string[] args)
         {
-            //Vereinbarung einer Variable zahl für ganze Zahlen
-            int zahl;
-            //1. Zuweisung
-            zahl = 10;
-            //1. Ausgabe
-            Console.WriteLine("zahl hat den Wert {0}", zahl);
-            //2. Ausgabe
-            Console.WriteLine($"zahl hat den Wert {zahl}");
-            //2. Zuweisung
-            zahl = 22;
-            Console.WriteLine("zahl hat jetzt den Wert {0}", zahl);
+            //Vereinbarung der Variablen
+            int zahl1 = 1;
+            int zahl2, zahl3, zahl4;
+            //Zuweisungen
+            //zahl2 erhält das Ergebnis des Audrucks 1+1, also 2
+            zahl2 = 1 + 1;
+            //zahl3 erhält den aktuellen Wert von Zahl2 * 5, also
+            //10
+            zahl3 = zahl2 * 5;
+
+            //Ausgaben
+            Console.WriteLine("zahl1 hat den Wert {0}", zahl1);
+            Console.WriteLine("zahl2 hat den Wert {0}", zahl2);
+            Console.WriteLine("zahl3 hat den Wert {0}", zahl3);
+
+            //Hier beginnt der Ringtausch
+            //Die Werte von zahl1 und zahl2 werden getauscht
+            //Zuerst wird der Wert von zahl1 in zahl4 gesichert
+            zahl4 = zahl1;
+            //dann erhält zahl1 den Wert von zahl2
+            zahl1 = zahl2;
+            //und zum Schluss wird zahl2 der gesicherte
+            //Wert von zahl4 zugewiesen
+            zahl2 = zahl4;
+
+            //Ausgaben nach dem Tausch
+            Console.WriteLine("zahl1 hat jetzt den Wert {0}", zahl1);
+            Console.WriteLine("zahl2 hat jetzt den Wert {0}", zahl2);
+
+
 
         }
     }
