e9cd568478c161fd308cb0d64b05124bfdf4963e
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-10-28 09:25:18 +0100

    3.2 Variablen

---
+++ CSHP02D 3.2 Variablen/CSHP02D 3.2 Variablen/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Variablen Einführung */
+
+using System;
+
+namespace CSHP02D_3._2_Variablen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vereinbarung einer Variable zahl für ganze Zahlen
+            int zahl;
+            //1. Zuweisung
+            zahl = 10;
+            //1. Ausgabe
+            Console.WriteLine("zahl hat den Wert {0}", zahl);
+            //2. Ausgabe
+            Console.WriteLine("zahl hat den Wert {0}", zahl);
+            //2. Zuweisung
+            zahl = 22;
+            Console.WriteLine("zahl hat jetzt den Wert {0}", zahl);
+
+        }
+    }
+}
