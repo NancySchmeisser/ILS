00d673a6bd7184309d613d806ca3d5d73292291c
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-10-29 10:07:08 +0100

    added symbolische Konstanten

---
+++ CSHP02D/Symbolische Konstanten.cs
@@ -0,0 +1,31 @@
+﻿/* Symbolische Konstanten
+ (das Programm lässt sich nicht übersetzen) */
+
+using System;
+
+namespace Cshp02d_03_06
+{
+    class Programm
+    {
+        static void Main(string [] args)
+        {
+            //die Vereinbarung der symbolischen Konstanten
+            const int konstante = 10;
+            //Vereinbarung der Variablen
+            int zahl2 = 15;
+            int ausgabe;
+
+            //Ausgabe der Konstante
+            //bitte in einer Zeile eingeben
+            Console.WriteLine("Konstante hat den Wert {0}", konstante);
+            //Rechnung mit der Konstanten
+            ausgabe = zahl2 + konstante;
+            // Ausgabe des Ergebnisses
+            Console.WriteLine("ausgabe hat den Wert {0}", ausgabe);
+            //DAS GEHT NICHT!
+            konstante = 5;
+        }
+    }
+}
+
+
--- code 3.5/code 3.5/Program.cs
+++ code 3.5/code 3.5/Program.cs
@@ -7,7 +7,7 @@ namespace code_3._5
         static void Main(string[] args)
         {
             int zahl = 5;
-            int ausgabe;
+            int ausgabe1;
 
             //einfaches Inkrement
             Console.WriteLine("zahl hat den Wert {0}", zahl);
@@ -16,15 +16,15 @@ namespace code_3._5
             Console.WriteLine("zahl hat nach ++zahl den Wert {0}\n", zahl);
 
             //Inkrement nach der Zuweisung
-            ausgabe = zahl++;
+            ausgabe1                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 = zahl++;
             //bitte jeweils in einer Zeile eingeben
-            Console.WriteLine("ausgabe hat den Wert {0}", ausgabe);
+            Console.WriteLine("ausgabe hat den Wert {0}", ausgabe1);
             Console.WriteLine("zahl hat jetzt den Wert {0}\n", zahl);
 
             //Inkrement vor der Zuweisung
-            ausgabe = ++zahl;
+            ausgabe1 = ++zahl;
             //bitte jeweils in einer Zeile eingeben
-            Console.WriteLine("ausgabe hat den Wert {0}", ausgabe);
+            Console.WriteLine("ausgabe hat den Wert {0}", ausgabe1);
             Console.WriteLine("zahl hat jetzt den Wert {0}\n", zahl);
 
             //entspricht zahl = zahl / 7
@@ -38,7 +38,21 @@ namespace code_3._5
 
 
 
+            //die Vereinbarung der symbolischen Konstanten
+            const int konstante = 20;
+            //Vereinbarung der Variablen
+            int zahl2 = 15;
+            int ausgabe;
 
+            //Ausgabe der Konstante
+            //bitte in einer Zeile eingeben
+            Console.WriteLine("Konstante hat den Wert {0}", konstante);
+            //Rechnung mit der Konstanten
+            ausgabe = zahl2 + konstante;
+            // Ausgabe des Ergebnisses
+            Console.WriteLine("ausgabe hat den Wert {0}", ausgabe);
+            //DAS GEHT NICHT!
+            //konstante = 5;
         }
     }
 }
