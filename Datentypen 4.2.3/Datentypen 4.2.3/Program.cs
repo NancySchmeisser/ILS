d157e09a1899738ed4f3c839847e708dbaff022e
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-03 10:59:30 +0100

    added string, char

--- Datentypen 4.2.3/Datentypen 4.2.3/Program.cs
+++ Datentypen 4.2.3/Datentypen 4.2.3/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿/* Die Datentypen string und char */
+
+using System;
 
 namespace Datentypen_4._2._3
 {
@@ -6,7 +8,24 @@ namespace Datentypen_4._2._3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Vereinbarung der Variablen
+            char charVariable;
+            string stringVariable;
+            //Werte zuweisen und ausgeben
+            charVariable = 'A';
+            //Das klappt nicht
+            //charVariable = "A";
+            stringVariable = "Ich bin eine Zeichenkette";
+
+            Console.WriteLine("Das Zeichen: {0}", charVariable);
+            Console.WriteLine("Die Zeichen: {0}", stringVariable);
+
+            stringVariable = "Zeichenketten lassen sich ändern!";
+            Console.WriteLine("Die Zeichenkette: {0}", stringVariable);
+
+            stringVariable = "Auch das " + "Verketten funktionert!";
+            Console.WriteLine("Die Zeichenkette: {0}", stringVariable);
+
         }
     }
 }
