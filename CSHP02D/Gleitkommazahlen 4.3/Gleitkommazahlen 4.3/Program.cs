eaaa5ac0d9a58b2256a6bbd4a1217e0df57b3567
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-03 09:33:58 +0100

    Zahlenformatzeichenfolge

--- CSHP02D/Gleitkommazahlen 4.3/Gleitkommazahlen 4.3/Program.cs
+++ CSHP02D/Gleitkommazahlen 4.3/Gleitkommazahlen 4.3/Program.cs
@@ -36,6 +36,8 @@ namespace Gleitkommazahlen
             //jetzt verändert sich die Genauigkeit der Nachkommastellen
             floatVariable = 1001.33333333333333333333F;
             Console.WriteLine("Als float: {0:G18}", floatVariable);
+            Console.WriteLine("Große Zahlen (10000000000) : {0:N0}", floatVariable);
+            Console.WriteLine("Große Zahlen (10000000000) : {0,20:N0}", floatVariable);
         }
     }
 }
