b4fed68f0738391479ca1eede4b6e654abd3807c
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-03 13:32:39 +0100

    4.3

---
+++ Datentyp für logische Werte/Datentyp für logische Werte/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Der Datentyp bool */
+
+using System;
+
+namespace Datentyp_für_logische_Werte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vereinbarung der Variablen
+            bool boolVariable;
+
+            //Werte zuweisen und ausgeben
+            boolVariable = true;
+            Console.WriteLine("Der Wert: {0}", boolVariable);
+
+            boolVariable = false;
+            Console.WriteLine("Der Wert: {0}", boolVariable);
+        }
+    }
+}
