dbec20e6e39a88c350ee4a6b4fafb9d948b064db
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-04 09:09:34 +0100

    added 5.1

---
+++ Variablen 5.2/Variablen 5.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Variablen_5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long longZahl = 1234567891234567;
+            int intZahl = 100;
+            double doubleZahl = 1.2;
+            byte byteZahl = 10;
+            //diese Zuweisungen sind möglich
+            longZahl = intZahl;
+            doubleZahl = intZahl;
+            doubleZahl = longZahl;
+            longZahl = byteZahl;
+            //diese dagegen nicht
+            //intZahl = longZahl;
+            //intZahl = doubleZahl;
+            //byteZahl = intZahl;
+
+
+
+        }
+    }
+}
