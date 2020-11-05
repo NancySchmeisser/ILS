d55ee1aa1720a80dfb51de257e3d58cc5d818c8e
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-05 12:59:57 +0100

    exercise page 62

--- Typecasting 2/Typecasting 2/Program.cs
+++ Typecasting 2/Typecasting 2/Program.cs
@@ -12,14 +12,14 @@ namespace Typecasting_5._3
 
             //Wertzuweisungen
             intVariable1 = 10;
-            doubleVariable1 = 10;
+            doubleVariable1 = 1.444444444444444;
             intVariable2 = 3;
             doubleVariable2 = 3;
 
             //beide double werden zum int, das Ergebnis wird falsch
             //bitte jweils in einer Zeile eingeben
-            doubleVariable3 = (int)doubleVariable1 / (int)doubleVariable2;
-            Console.WriteLine("(int) double / (int) double = {0}", doubleVariable3);
+           //doubleVariable3 = (int)doubleVariable1 / (int)doubleVariable2;
+            //Console.WriteLine("(int) double / (int) double = {0}", doubleVariable3);
 
             //ein int wird zum double, das Ergebnis stimmt
             doubleVariable3 = (double)intVariable1 / intVariable2;
@@ -36,6 +36,10 @@ namespace Typecasting_5._3
 
             //bitte in einer Zeile eingeben
             Console.WriteLine("(double)  (int / int) = {0}", doubleVariable3);
+
+            //float float1 = doubleVariable1;
+            //Aufgaben
+            Console.WriteLine("Ergebnis = {0}", (float)doubleVariable1);
 
         }
     }
