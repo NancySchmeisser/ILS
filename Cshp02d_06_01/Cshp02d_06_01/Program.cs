835ea58fe1759de81bd30bdd9e706850723bc82c
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-06 11:55:54 +0100

    added converts

--- Cshp02d_06_01/Cshp02d_06_01/Program.cs
+++ Cshp02d_06_01/Cshp02d_06_01/Program.cs
@@ -7,39 +7,36 @@ namespace Cshp02d_06_01
         static void Main(string[] args)
         {
             //Vereinbarung der Variablen
-            int eingabe1;
-            string eingabe2, eingabe3;
+            char eingabe1;
+            int eingabe2, eingabe3;
 
             //ein Zeichen über Read() lesen und ausgeben
             //bitte in einer Zeile eingeben
             Console.WriteLine("Geben sie ein Zeichen ein. Drücken sie dann die Eingabetaste");
-            eingabe1 = Console.Read();
+            eingabe1 = Convert.ToChar(Console.Read());
 
             //bitte in einer Zeile eingeben
             Console.WriteLine("Sie haben das Zeichen {0} eingegeben", eingabe1);
-            //die Eingabetaste aus dem Puffer holen
-            //sonst wird die nächste Eingabe scheinbar übersprungen
-            Console.ReadLine();
 
             //Zahlen einlesen
             //bitte in einer Zeile eingeben
             Console.Write("Geben sie bitte die erste Zahl ein. Drücken Sie dann die Eingabetaste. ");
-            eingabe2 = Console.ReadLine();
-            
+            eingabe2 = Convert.ToInt32(Console.ReadLine());
+
             //bitte in einer Zeile eingeben
 
             Console.Write("Geben sie bitte die zweite Zahl ein. Drücken Sie dann die Eingabetaste. ");
-            eingabe3 = Console.ReadLine();
+            eingabe3 = Convert.ToInt32(Console.ReadLine());
 
             //bitte in einer Zeile eingeben
             Console.WriteLine(" Die Addition der Zahlen {0} und {1} ergibt {2}", eingabe2, eingabe3, eingabe2 + eingabe3);
 
             //Zeichenketten einlesen
             Console.Write("Wie heißen Sie ");
-            eingabe2 = Console.ReadLine();
+            //eingabe2 = Console.ReadLine();
 
             Console.Write("Wen wollen sie grüßen? ");
-            eingabe3 = Console.ReadLine();
+            //eingabe3 = Console.ReadLine();
 
             //bitte in einer Zeile eingeben
             Console.WriteLine("Hallo {0}. Es grüßt Dich {1}", eingabe3, eingabe2);
