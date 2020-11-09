3df3ba5895ab4e30c327cb356e013aea0fe84ddb
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-09 09:04:20 +0100

    added 7.1

---
+++ CSHP02D_7.1/CSHP02D_7.1/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Gestaltung von Konsolenprogrammen */
+
+using System;
+
+namespace CSHP02D_7._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vereinbarung der Variablen
+            string eingabe1, eingabe2;
+
+            //den Titel setzten
+            Console.Title = "Nun sieht es netter aus!";
+
+            //die Farben verändern
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+        }
+    }
+}
