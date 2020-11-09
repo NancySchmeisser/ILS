45b744260254b69edd972872e6886a9864cd1f8b
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-09 14:24:38 +0100

    added 1.2

---
+++ CSHP03D 1.2/CSHP03D 1.2/Program.cs
@@ -0,0 +1,16 @@
+﻿/* ##################################
+ Logisches NICHT
+##################################### */
+
+using System;
+
+namespace CSHP03D_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int zahl1, zahl2;
+        }
+    }
+}
