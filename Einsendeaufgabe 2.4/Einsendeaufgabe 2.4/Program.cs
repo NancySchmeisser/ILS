197d4729cd3a36a0b1313df7fd6a6cac4d2485eb
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-09 11:30:29 +0100

    Einsendeaufgabe 2.4

--- Einsendeaufgabe 2.4/Einsendeaufgabe 2.4/Program.cs
+++ Einsendeaufgabe 2.4/Einsendeaufgabe 2.4/Program.cs
@@ -11,12 +11,16 @@ namespace Aufgabe0204
 		static void Main(string[] args)
 		{
 			int variable;
-			variable = 60;
+            const int minutenProStunde = 60;
+			const int stundenProTag = 24;
+			const int tageProWoche = 7;
+
+            variable = minutenProStunde;
 			//bitte in einer Zeile eingeben
 			Console.WriteLine("Eine Stunde hat {0} Minuten.", variable);
-			variable = 60 * 24;
+			variable = minutenProStunde * stundenProTag;
 			Console.WriteLine("Ein Tag hat {0} Minuten.", variable);
-			variable = 60 * 24 * 7;
+			variable = minutenProStunde * stundenProTag * tageProWoche;
 			//bitte in einer Zeile eingeben
 			Console.WriteLine("Eine Woche hat {0} Minuten.", variable);
 		}
