03b3f5fd040b14f57670d71a27875d576f4e0c1c
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-09 10:32:43 +0100

    added Code 2.4

---
+++ Einsendeaufgabe 2.4/Einsendeaufgabe 2.4/Program.cs
@@ -0,0 +1,24 @@
+﻿/* #################################################
+Einsendeaufgabe 2.4
+################################################# */
+
+using System;
+
+namespace Aufgabe0204
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int variable;
+			variable = 60;
+			//bitte in einer Zeile eingeben
+			Console.WriteLine("Eine Stunde hat {0} Minuten.", variable);
+			variable = 60 * 24;
+			Console.WriteLine("Ein Tag hat {0} Minuten.", variable);
+			variable = 60 * 24 * 7;
+			//bitte in einer Zeile eingeben
+			Console.WriteLine("Eine Woche hat {0} Minuten.", variable);
+		}
+	}
+}
