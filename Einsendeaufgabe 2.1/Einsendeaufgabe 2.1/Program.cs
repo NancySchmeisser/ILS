c362365a9ea15abb4a88a69416716d7a3e6ac657
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-09 09:39:05 +0100

    Einsendeaufgabe 2.1/ 1. Aufgabe

---
+++ Einsendeaufgabe 2.1/Einsendeaufgabe 2.1/Program.cs
@@ -0,0 +1,30 @@
+﻿/* #########################################################
+ Einsendeaufgabe 2.1
+######################################################### */
+
+
+using System;
+
+namespace Einsendeaufgabe_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\"\t");
+            Console.WriteLine("#\t\t#");
+            Console.WriteLine("#\t\t#");
+            Console.WriteLine("#\t\t#");
+            Console.WriteLine("#\t\t#");
+            Console.WriteLine("#\t\t#");
+
+            Console.WriteLine("\t\"\t");
+            Console.WriteLine("#\t\t#");
+            Console.WriteLine("#\t\t#");
+            Console.WriteLine("#\t\t#");
+            Console.WriteLine("#\t\t#");
+            Console.WriteLine("#\t\t#");
+            Console.WriteLine("\t\"\t");
+        }
+    }
+}
