f6bd4f9a30c0bd01ff5677b8e991a23ddc8af431
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-10 10:51:01 +0100

    added code 1.3

---
+++ CSHP03D 1.3/CSHP03D 1.3/Program.cs
@@ -0,0 +1,20 @@
+﻿/* ##################################
+ Logisches ODER
+##################################### */
+
+using System;
+
+namespace CSHP03D_1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A \t B \t\t A || B");
+            Console.WriteLine("wahr \t wahr \t\t {0}", (true || true ));
+            Console.WriteLine("wahr \t falsch \t\t {0}", (true || false));
+            Console.WriteLine("falsch \t wahr \t\t {0}", (false || true));
+            Console.WriteLine("falsch \t falsch \t\t {0}", (false || false));
+        }
+    }
+}
