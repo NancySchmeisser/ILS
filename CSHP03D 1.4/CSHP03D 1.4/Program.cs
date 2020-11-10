8a7b87eaf798dcdd0a72dd988a93c466d8a1f207
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-10 11:08:18 +0100

    added code 1.4

---
+++ CSHP03D 1.4/CSHP03D 1.4/Program.cs
@@ -0,0 +1,20 @@
+﻿/* ##################################
+ Logisches UND
+##################################### */
+
+using System;
+
+namespace CSHP03D_1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A \t B \t\t A && B");
+            Console.WriteLine("wahr \t wahr \t\t {0}", (true && true));
+            Console.WriteLine("wahr \t falsch \t\t {0}", (true && false));
+            Console.WriteLine("falsch \t wahr \t\t {0}", (false && true));
+            Console.WriteLine("falsch \t falsch \t\t {0}", (false && false));
+        }
+    }
+}
