a50254b1edbf3c37da9023139b95a8a50b382495
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-10 08:43:24 +0100

    added code 1.2

--- CSHP03D 1.2/CSHP03D 1.2/Program.cs
+++ CSHP03D 1.2/CSHP03D 1.2/Program.cs
@@ -11,6 +11,20 @@ namespace CSHP03D_1._2
         static void Main(string[] args)
         {
             int zahl1, zahl2;
+            zahl1 = 1;
+            zahl2 = 2;
+
+            //ein direkter Vergleich von zwei Zahlen
+            Console.WriteLine("!(5 < 10) \t {0}", !(5 <10));
+
+            //bitte jeweils in einer Zeile eingeben
+            //ein Vergleich einer Zahl mit einer Variablen
+            Console.WriteLine("!(5 > {0}) \t{1}", zahl1, !(5 > zahl1));
+
+            //Vergleiche von 2 Variablen
+            Console.WriteLine("!({0} == {1}) \t{2}", zahl1, zahl2, !(zahl1 ==zahl2));
+
+            Console.WriteLine("!({0} != {1}) \t{2}", zahl1, zahl2, !(zahl1 != zahl2));
         }
     }
 }
