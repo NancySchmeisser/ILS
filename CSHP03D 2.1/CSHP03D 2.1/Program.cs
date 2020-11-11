d1b693ad6e93180d46613405ec39fc3f03361651
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-11 10:04:42 +0100

    added code 2.1

---
+++ CSHP03D 2.1/CSHP03D 2.1/Program.cs
@@ -0,0 +1,26 @@
+﻿/* ########################
+  Einfache if-Verzweigung
+##########################*/
+
+using System;
+
+namespace CSHP03D_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int zahl;
+
+            Console.Write("Bitte geben Sie eine Zahl ein: ");
+            zahl = Convert.ToInt32(Console.ReadLine());
+
+            //die Verzweigung
+            if (zahl > 5)
+            
+                Console.WriteLine("Sie haben eine Zahl größer als 5 eingegeben");
+                Console.WriteLine("Die Zahl war {0}.", zahl);
+            
+        }
+    }
+}
