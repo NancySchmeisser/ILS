077b4e5544c2d6e34baa324d0ce1bdfb74d8d3ca
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-12 09:54:43 +0100

    added code 2.6

--- CSHP03D 2.5/CSHP03D 2.5/Program.cs
+++ CSHP03D 2.5/CSHP03D 2.5/Program.cs
@@ -10,15 +10,24 @@ namespace CSHP03D_2._5
     {
         static void Main(string[] args)
         {
-            char essenWahl;
+            char essenWahl, beilagenWahl;
 
             Console.WriteLine("Sie haben folgende Auswahl: \n");
             Console.WriteLine("a Schweineschnitzel");
             Console.WriteLine("b Wiener Schnitzel");
-          
+
             Console.Write("Was möchten Sie essen? ");
 
             essenWahl = Convert.ToChar(Console.Read());
+            //den Tastaturpuffer leeren
+            Console.ReadLine();
+
+            Console.WriteLine("Sie können folgende Beilagen wählen: \n");
+            Console.WriteLine("c Pommes");
+            Console.WriteLine("d Reis \n");
+            Console.WriteLine("Welche Beilage möchten Sie?");
+
+            beilagenWahl = Convert.ToChar(Console.Read());
 
             //Die Auswertung von essenWahl
             switch (essenWahl)
@@ -26,28 +35,54 @@ namespace CSHP03D_2._5
                 //der case-Zweig für a
                 case 'A':
                 case 'a':
-                    Console.WriteLine("Sie haben Azad gewählt!");
-                    Console.WriteLine("Davon wirst du garantiert fett!");
-                    break;
-                //der case-Zweig für b
-                case 'B':
-                case 'b':
-                    Console.WriteLine("Sie haben Maykels gewählt!");
-                    Console.WriteLine("Da gibt es durchaus auch was Leichtes, dauert aber!");
-                    break;
-                //der case-Zweig für c
+
+                //die Auswertung von beilagenWahl
+               switch (beilagenWahl) 
+                {
                 case 'C':
                 case 'c':
-                    Console.WriteLine("Sie haben Chinese gewählt!");
-                    Console.WriteLine("Hattest du gestern erst, aber was solls!");
+
+                    Console.WriteLine("Sie haben Schweineschnitzel mit Pommes gewählt!");
                     break;
-                //für alles andere
+                case 'D':
+                case 'd':
+                    Console.WriteLine("Sie haben Schweineschnitzel mit Reis gewählt!");
+                    break;
                 default:
-                    Console.WriteLine("Sie haben keine gültige Auswahl getroffen!");
-                    Console.WriteLine("Sie werden in der Hölle schmoren, wenn sie ihr Nancy nicht füttern!");
+                    Console.WriteLine("Diese Beilage gibt es nicht.");
                     break;
+            }  //Hier endet die Auswertung von beilagenwahl
+            
+            break; //hier enden die case-Marken für a und A
+
+                    
+                    
+                case 'B':
+                case 'b':
+                    //die Auswertung von beilagenWahl
+                
+                switch (beilagenWahl) 
+            { 
+                case 'C':
+                case 'c':
+
+                    Console.WriteLine("Sie haben Wiener Schnitzel mit Pommes gewählt!");
+            break;
+                case 'D':
+                case 'd':
+                    Console.WriteLine("Sie haben Wiener Schnitzel mit Reis gewählt!");
+            break;
+            default:
+                    Console.WriteLine("Diese Beilage gibt es nicht.");
+            break;
+        }//hier endet die Asuwertung von beilagenWahl
+        break; //hier enden die case-Marken für b und B
+        default: //für die Auswertung von essenWahl
+        Console.WriteLine("Dieses Gericht steht nicht auf der Karte.");
+        break;
+            }
             }
 
         }
     }
-}
+
