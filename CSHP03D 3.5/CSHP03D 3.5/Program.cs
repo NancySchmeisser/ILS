c22de761a31b8b380813e00c145444f1464f2fe6
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-13 09:11:50 +0100

    added code 3.5

---
+++ CSHP03D 3.5/CSHP03D 3.5/Program.cs
@@ -0,0 +1,18 @@
+﻿/* #################
+ for-Schleife
+###################*/
+
+using System;
+
+namespace CSHP03D_3._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0; i <= 10; i++)
+                Console.WriteLine("Die Variable hat jetzt den Wert {0}", i);
+        }
+    }
+}
