14ad3c19f30a6e10374e9813e28dff3cc6720add
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-13 10:04:21 +0100

    added code 3.9

--- CSHP03D 3.8/CSHP03D 3.8/Program.cs
+++ CSHP03D 3.8/CSHP03D 3.8/Program.cs
@@ -11,21 +11,24 @@ namespace CSHP03D_3._8
         static void Main(string[] args)
         {
             int summe, schleifenVariable, eingabe;
+            bool flagVariable = true;
 
             summe = 0;
             schleifenVariable = 1;
 
-            while(schleifenVariable <= 10)
+            while(schleifenVariable <= 10 && (flagVariable ==true))
             {
 
                 Console.Write("Geben Sie die {0}. Zahl ein: ", schleifenVariable);
                 eingabe = Convert.ToInt32(Console.ReadLine());
 
                 if (eingabe == 0)
-                    break;
-                summe = eingabe + summe;
-                schleifenVariable++;
-
+                    flagVariable = false;
+                else
+                {
+                    summe = eingabe + summe;
+                    schleifenVariable++;
+                }
             }
             Console.WriteLine("Das Einlesen ist beendet.");
             Console.WriteLine("Die Summe der Zahlen ist {0}.", summe);
