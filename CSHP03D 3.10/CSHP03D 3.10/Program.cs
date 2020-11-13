5a2e0fdbeff26288f4898808b223693b38a8baa4
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-13 10:18:51 +0100

    added code 3.10

---
+++ CSHP03D 3.10/CSHP03D 3.10/Program.cs
@@ -0,0 +1,33 @@
+﻿/* ########################
+ continue in einer Schleife
+##########################*/
+
+using System;
+
+namespace CSHP03D_3._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quadrat, schleifenVariable, eingabe;
+
+            quadrat = 0;
+            schleifenVariable = 1;
+
+            while (schleifenVariable <= 10) 
+            { 
+                Console.WriteLine("Geben sie die Zahl {0} ein", schleifenVariable);
+            eingabe = Convert.ToInt32(Console.ReadLine());
+
+            schleifenVariable++;
+            if (eingabe == 0)
+                continue;
+            quadrat = eingabe * eingabe;
+            Console.WriteLine("Das Quadrat der Zahl ist {0}", quadrat);
+            }
+        
+         Console.WriteLine("Das Einlesen ist beendet.");   
+        }
+    }
+}
