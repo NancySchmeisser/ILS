dd7eb90cf5eed6438d976c2d66bc01d50145114e
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-13 08:18:52 +0100

    added code 3.1

---
+++ CSHP03D 3.1/CSHP03D 3.1/Program.cs
@@ -0,0 +1,24 @@
+﻿/* #################
+ while-Schleife
+###################*/
+
+
+using System;
+
+namespace CSHP03D_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int schleifenVariable = 0;
+
+            //die Schleife
+            while (schleifenVariable <= 10) ;
+            {
+                Console.WriteLine("Die Variable hat jetzt den Wert {0}", schleifenVariable);
+                schleifenVariable++;
+            }
+        }
+    }
+}
