305c3d67ab57d031a1bdb477c67c0486eb58fac3
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-16 10:56:44 +0100

    added code 4.3

---
+++ CSHP03D 4.3/CSHP03D 4.3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CSHP03D_4._3
+{
+    class Program
+    {
+        static int Rueckgabe100()
+        {
+            return 100;
+        }
+
+        static int Rueckgabe10()
+        {
+            return 10;
+        }
+
+        static void Main(string[] args)
+        {
+            int ergebnis;
+
+            Console.WriteLine("Die Methode Rueckgabe100() liefert den Wert {0}.", Rueckgabe100());
+            Console.WriteLine("Die Methode Rueckgabe10() liefert den Wert {0}.", Rueckgabe10());
+
+            ergebnis = Rueckgabe100() + Rueckgabe10();
+            Console.WriteLine("Das Ergebnis ist {0}.", ergebnis);
+
+            Console.Write("Geben Sie eine Zahl ein: ");
+            ergebnis = Convert.ToInt32(Console.ReadLine());
+            if (ergebnis < Rueckgabe100())
+                Console.WriteLine("Ihr Eingabe war kleiner als 100");
+
+        }
+
+    }
+}
