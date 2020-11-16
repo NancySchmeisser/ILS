bbbfbd83305aeb3b08d079f9d586baf21afb0137
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-16 09:15:38 +0100

    added code 4.1

---
+++ CSHP03D 4.1/CSHP03D 4.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSHP03D_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# macht Spaß");
+            Console.WriteLine("Aber nicht immer");
+
+        }
+    }
+}
