9989659f1c1f099a418fa2dd18b678ad2042944d
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-17 09:25:20 +0100

    added code 4.4

---
+++ CSHP03D code 4.4/CSHP03D code 4.4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSHP03D_code_4._4
+{
+    class Program
+    {
+        static int Eingabe()
+        {
+            int einVariable;
+            Console.Write("Geben Sie eine Zahl ein: ");
+            einVariable = Convert.ToInt32(Console.ReadLine());
+            if (einVariable <= 20)
+            return einVariable;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Die Methode Eingabe() liefert den Wert {0}.", Eingabe());
+        }
+
+    }
+}
