86163a0f55985b124afd144f1e2e46578ad1e490
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-17 11:41:25 +0100

    exercise

--- CSHP03D 4.8/CSHP03D 4.8/Program.cs
+++ CSHP03D 4.8/CSHP03D 4.8/Program.cs
@@ -1,17 +1,19 @@
 ﻿using System;
+using System.Linq;
 
 namespace CSHP03D_4._8
 {
     class Program
     {
-       static int Summe (int x, int y )
+        static int Summe (int x, int y, int z )
         {
             
-            return (x + y);
+            return (x + y + z);
         }
         static void Main (String[] args)
         {
-            int einVariable1, einVariable2;
+           
+            int einVariable1, einVariable2, einVariable3;
 
             Console.Write("Geben Sie die erste Zahl ein: ");
             einVariable1 = Convert.ToInt32(Console.ReadLine());
@@ -19,9 +21,12 @@ namespace CSHP03D_4._8
             Console.Write("Geben Sie die zweite Zahl ein: ");
             einVariable2 = Convert.ToInt32(Console.ReadLine());
 
+            Console.Write("Geben Sie die dritte Zahl ein: ");
+            einVariable3 = Convert.ToInt32(Console.ReadLine());
 
 
-            Console.WriteLine("Die Summe der beiden Zahlen ist {0}", Summe(einVariable1, einVariable2));
+
+            Console.WriteLine("Die Summe der beiden Zahlen ist {0}", Summe(einVariable1, einVariable2, einVariable3));
         }
     }
 }
