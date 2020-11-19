48f46c2a08ece09f1e583e3af7c336685da1fb8c
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-19 10:34:27 +0100

    Einsendeaufgabe 3.2

---
+++ Einsendeaufgabe 3.2/Einsendeaufgabe 3.2/Program.cs
@@ -0,0 +1,23 @@
+﻿/* #################################################### 
+Einsendeaufgabe 3.2 
+#################################################### */
+
+
+using System;
+
+namespace Einsendeaufgabe_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            for (int i = 1; i <= 5; i++)
+            {
+                if (i != 1)
+                    Console.Write(",");
+                Console.Write("{0}", i);
+            }
+        }
+    }
+}
