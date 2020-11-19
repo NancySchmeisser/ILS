7f47b44da497a7d09eaf1fe86ce4cbaf89518b57
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-19 11:18:01 +0100

    Einsendeaufgabe 3.3

---
+++ Einsendeaufgabe 3.3/Einsendeaufgabe 3.3/Program.cs
@@ -0,0 +1,23 @@
+﻿/* #################################################### 
+Einsendeaufgabe 3.3 
+#################################################### */
+
+
+using System;
+
+namespace Einsendeaufgabe_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int schleifenVariable = 1;
+
+            while (schleifenVariable <= 25)
+            {
+                Console.WriteLine("Zahl = \t {0}", schleifenVariable *2);
+                schleifenVariable++;
+            }
+        }
+    }
+}
