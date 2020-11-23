54e7a81da749d24340fb433f8a078531f89ab463
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-23 09:54:21 +0100

    added code 1.1

---
+++ CSHP04D 1.1/CSHP04D 1.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSHP04D_1._1
+{
+    class Program
+    {
+        static void GibVarAus()
+        {
+            int varA = 4;
+            Console.WriteLine("Der Wert der Variablen ist {0}", varA);
+        }
+
+        static void Main(string[] args)
+        {
+            GibVarAus();
+        }
+    }
+}
