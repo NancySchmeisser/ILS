4efecfe0d491930d3ed467ea38e4056f41f7cf8c
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-24 08:46:23 +0100

    added code 1.6

---
+++ CSHP04D 1.6/CSHP04D 1.6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSHP04D_1._6
+{
+    class Program
+    {
+       static void ErhoeheA(int a)
+        {
+            a = a + 1;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine("Die Variable hat den Wert {0}", a);
+            ErhoeheA(a);
+            Console.WriteLine("Die Variable hat den Wert {0}", a);
+        }
+    }
+}
