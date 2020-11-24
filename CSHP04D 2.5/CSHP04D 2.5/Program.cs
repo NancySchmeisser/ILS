57f9af387bfa6b7f844c042ed5e2dc484f675c0d
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-24 12:57:55 +0100

    added code 2.5

---
+++ CSHP04D 2.5/CSHP04D 2.5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSHP04D_2._5
+{
+    class Program
+    {
+        static void Eingabe(int[] argArray)
+        {
+            for (int index = 0; index < argArray.Length; index++)
+            {
+                Console.Write("Bitte geben Sie die {0}. Zahl ein: ", index + 1);
+                argArray[index] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        static void Quadrat(int[] argArray)
+        {
+            foreach (int element in argArray)
+                Console.WriteLine("Das Quadrat von {0} ist: {1}", element, element * element);
+        }
+
+        static void Main (string[] args)
+        {
+            int[] zahlenArray = new int[5];
+            Eingabe(zahlenArray);
+            Quadrat(zahlenArray);
+        }
+    }
+}
