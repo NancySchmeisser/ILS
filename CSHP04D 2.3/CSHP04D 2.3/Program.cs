828c57426bb7eedb171d2672dfc054be29def75c
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-24 10:50:57 +0100

    added code 2.3

---
+++ CSHP04D 2.3/CSHP04D 2.3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSHP04D_2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] zahlen = new int[10];
+            for (int element = 0; element < zahlen.Length; element++)
+            zahlen[element] = element * element;
+            foreach (int element in zahlen)
+                Console.WriteLine("Der Wert ist {0}.", element);
+        }
+    }
+}
