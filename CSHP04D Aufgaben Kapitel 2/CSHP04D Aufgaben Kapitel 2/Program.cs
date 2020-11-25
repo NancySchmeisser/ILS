c5e4da1b07ffe14176860b8d57532e69cfb1d85a
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-25 11:05:46 +0100

    added code 2.2 exercise

---
+++ CSHP04D Aufgaben Kapitel 2/CSHP04D Aufgaben Kapitel 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSHP04D_Aufgaben_Kapitel_2
+{
+    class Program
+    {
+
+        static void Eingabe(int argArray[4])
+        {
+            for(int i =0; i < argArray.Length; i++)
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            float[] testArray = new float[5];
+            Eingabe();
+        }
+    }
+}
