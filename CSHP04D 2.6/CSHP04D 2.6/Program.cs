d0507c1cea575c61a353783f8b83b33be9e9a37c
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-25 09:20:17 +0100

    added code 2.6

---
+++ CSHP04D 2.6/CSHP04D 2.6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CSHP04D_2._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] aArray;
+            int[] bArray = new int[5];
+
+            for (int element = 0; element < bArray.Length; element++)
+            bArray[element] = 5 * element;
+
+            aArray = bArray;
+            Console.WriteLine("Die Werte in aArray sind:");
+            foreach (int element in aArray)
+                Console.WriteLine("{0}", element);
+
+            Console.WriteLine("Die Werte in bArray sind:");
+            foreach (int element in bArray)
+                Console.WriteLine("{0}", element);
+
+            aArray[0] = 200;
+            bArray[4] = 1000;
+
+            Console.WriteLine("Die Werte in aArray sind:");
+            foreach (int element in aArray)
+                Console.WriteLine("{0}", element);
+
+            Console.WriteLine("Die Werte in bArray sind:");
+            foreach (int element in bArray)
+                Console.WriteLine("{0}", element);
+        }
+    }
+}
