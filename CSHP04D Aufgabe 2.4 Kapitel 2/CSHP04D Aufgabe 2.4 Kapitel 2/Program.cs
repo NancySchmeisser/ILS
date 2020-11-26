42c50760d50a57d3dbc5f6d1ee33914f2c93ea6b
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-26 10:49:16 +0100

    2.4 a

---
+++ CSHP04D Aufgabe 2.4 Kapitel 2/CSHP04D Aufgabe 2.4 Kapitel 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSHP04D_Aufgabe_2._4_Kapitel_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] test = {5, 7, 6, 8, 3, 9, 4, 1, 2, 0};
+
+            for (int index = 0; index < 10; index++)
+            Console.WriteLine("Der Wert des Elements {0} ist: {1}", index, test[index]);
+
+        }
+    }
+}
