f9760d230081c0da42fe3f0c8edbf2935e90524c
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-26 10:15:48 +0100

    CSHP04D Aufgabe 2.3 Kapitel 2

--- CSHP04D Aufgaben Kapitel 2/CSHP04D Aufgaben Kapitel 2/Program.cs
+++ CSHP04D Aufgaben Kapitel 2/CSHP04D Aufgaben Kapitel 2/Program.cs
@@ -14,8 +14,25 @@ namespace CSHP04D_Aufgaben_Kapitel_2
         }
         static void Main(string[] args)
         {
-            float[] testArray = new float[5];
-            Eingabe(testArray);
+            // float[] testArray = new float[5];
+            // Eingabe(testArray);
+
+            int[,,] r =
+            {
+                {
+                    { 1, 4, 6, 7 },
+                    { 2, 4, 0, 9 },
+                    { -4, 3, 6, 7 },
+                },
+                {
+                    { 5, 7, 8, -2 },
+                    { -3, 22, 6, 4 },
+                    { 0, -3, 2, 4 },
+                }
+            };
+
+            Console.WriteLine(r[0, 2, 3]);
+        
         }
     }
 }
