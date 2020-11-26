d4ebc2719461a41ea044f47b6a64eaadfe113071
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-26 09:30:48 +0100

    CSHP04D Aufgabe 2.2, Kapitel 2

--- CSHP04D Aufgaben Kapitel 2/CSHP04D Aufgaben Kapitel 2/Program.cs
+++ CSHP04D Aufgaben Kapitel 2/CSHP04D Aufgaben Kapitel 2/Program.cs
@@ -5,17 +5,17 @@ namespace CSHP04D_Aufgaben_Kapitel_2
     class Program
     {
 
-        static void Eingabe(int argArray[4])
+        static void Eingabe(float[] argArray)
         {
-            for(int i =0; i < argArray.Length; i++)
+            for(int i = 0; i < argArray.Length; i++) 
             {
-
+                Console.WriteLine("{0}", i + 1);
             }
         }
         static void Main(string[] args)
         {
             float[] testArray = new float[5];
-            Eingabe();
+            Eingabe(testArray);
         }
     }
 }
