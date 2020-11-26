387ff225809795ed3d7f39e577287a8cffc93073
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-26 11:06:24 +0100

    2.4 c

--- CSHP04D Aufgabe 2.4 Kapitel 2/CSHP04D Aufgabe 2.4 Kapitel 2/Program.cs
+++ CSHP04D Aufgabe 2.4 Kapitel 2/CSHP04D Aufgabe 2.4 Kapitel 2/Program.cs
@@ -8,7 +8,7 @@ namespace CSHP04D_Aufgabe_2._4_Kapitel_2
         {
             float[] test = {5, 7, 6, 8, 3, 9, 4, 1, 2, 0};
 
-            foreach (float element in test)
+            for (int element = 0; element <test.Length; element++)
                 Console.WriteLine("Der Wert ist {0}", element);
 
         }
