65a0bd6f852641b254c2c312559ced97de69f6a9
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-27 08:40:10 +0100

    added code 3.1

---
+++ CSHP04D 3.1/CSHP04D 3.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CSHP04D_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string zKette1;
+            string zKette2;
+            string zKette3;
+
+            zKette1 = "Ich bin";
+            zKette2 = " eine Zeichenkette";
+
+            zKette3 = zKette1 + zKette2;
+
+            Console.WriteLine("Die Zeichenkette enthält {0} Zeichen.", zKette3.Length);
+
+            for (int index = 0; index < zKette3.Length; index++)
+                Console.Write(" {0}", zKette3[index]);
+            Console.WriteLine();
+
+            foreach (char zeichen in zKette3)
+                Console.Write("{0}", zeichen);
+            Console.WriteLine();
+
+        }
+    }
+}
