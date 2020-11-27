5207e0cfb3abc92791c84c2cd02d9cb59ccbab9e
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-27 08:52:33 +0100

    added code 3.2

---
+++ CSHP04D 3.2/CSHP04D 3.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSHP04D_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string zKette2;
+            string zKette1 = "Rätsel";
+
+            do
+            {
+                Console.Write("Raten Sie. Wie heißt das Wort? ");
+                zKette2 = Console.ReadLine();
+            } while (zKette2 == String.Empty);
+
+            if (zKette1 == zKette2)
+                Console.WriteLine("Sie haben richtig geraten.");
+            else
+                Console.WriteLine("Leider falsch");
+        }
+    }
+}
