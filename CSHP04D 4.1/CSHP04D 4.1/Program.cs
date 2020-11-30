f6da1ef25d78e86fdbdb8053e69976a54efe699d
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-30 10:26:34 +0100

    added code 4.1

---
+++ CSHP04D 4.1/CSHP04D 4.1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CSHP04D_4._1
+{
+    class Program 
+    { 
+            struct Kiste
+            {
+                public int Hoehe;
+                public int Breite;
+                public int Laenge;
+            }
+
+            static Kiste Einlesen(int kistenNummer)
+            {
+                Kiste aKiste;
+                Console.WriteLine("Geben Sie die Höhe der Kiste {0}. ein: ", kistenNummer);
+                aKiste.Hoehe = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Geben Sie die Breite der Kiste {0}. ein: ", kistenNummer);
+                aKiste.Breite = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Geben Sie die Länge der Kiste {0}. ein: ", kistenNummer);
+                aKiste.Laenge = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine();
+                return aKiste;
+            }
+
+            static int Volumen(Kiste aKiste)
+            {
+                int volWert;
+                volWert = aKiste.Breite * aKiste.Laenge * aKiste.Hoehe;
+                return volWert;
+            }
+        
+        
+        static void Main(string[] args)
+        {
+            Kiste[] kleineKiste = new Kiste[3];
+
+            for (int index = 0; index < 3; index++)
+                kleineKiste[index] = Einlesen(index + 1);
+
+            for (int index = 0; index < 3; index++)
+                Console.WriteLine("Das Volumen von Kiste {0} ist: {1}", index + 1, Volumen(kleineKiste[index]));
+        }
+    }
+}
