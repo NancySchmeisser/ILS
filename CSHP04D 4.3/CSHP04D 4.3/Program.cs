ab7074a0b7bcc6a11dfae191cf41f3c0d86895d1
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-11-30 11:13:52 +0100

    added code 4.3

---
+++ CSHP04D 4.3/CSHP04D 4.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSHP04D_4._3
+{
+    class Program
+    {
+       
+        
+            enum Tage { Montag, Dienstag, Mittwoch, Donnerstag, Freitag, Samstag, Sonntag};
+
+        static void Main(string[] args)
+        {
+            Tage meineWoche;
+
+            meineWoche = Tage.Montag;
+
+            if (meineWoche == Tage.Montag)
+                Console.WriteLine("Heute ist Montag");
+            else
+                Console.WriteLine("Heute ist kein Montag");
+        }
+    }
+}
