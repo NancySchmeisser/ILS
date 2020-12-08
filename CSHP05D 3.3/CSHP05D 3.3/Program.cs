7a66db56bcf2bf292db9e2215d072a91dafebe4b
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-12-08 10:50:51 +0100

    added code 3.3

---
+++ CSHP05D 3.3/CSHP05D 3.3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CSHP05D_3._3
+{
+
+    class Autoklasse
+    {
+        static int autoZaehler;
+        int geschwindigkeit;
+
+
+        public static int GetAutoZaehler()
+        {
+            return autoZaehler;
+        }
+
+        public void Initialisiere(int standard)
+        {
+            geschwindigkeit = standard;
+            autoZaehler++;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Autoklasse auto1 = new Autoklasse();
+            Autoklasse auto2 = new Autoklasse();
+
+            auto1.Initialisiere(0);
+            auto2.Initialisiere(10);
+
+            Console.WriteLine("Die Anzahl der Autos ist {0}", Autoklasse.GetAutoZaehler());
+
+        }
+    }
+}
