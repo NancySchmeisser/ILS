0d836bb7730396ace850b05c4eb75f9008596ed0
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-12-08 10:20:12 +0100

    added code 3.2

---
+++ CSHP05D 3.2/CSHP05D 3.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSHP05D_3._2
+{
+    class Quadrat
+    {
+        static int ergebnis;
+
+        public static int Berechnen(int wert)
+        {
+            ergebnis = wert * wert;
+            return ergebnis;
+        }
+    }
+
+    class Program 
+    
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Das Quadrat von 4 ist {0}", Quadrat.Berechnen(4));  
+            
+        }
+    }
+}
