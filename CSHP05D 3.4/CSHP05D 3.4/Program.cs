0b2da98ef47f9d2c7fdd5f436d3026a7bb4e3cb2
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-12-10 08:23:25 +0100

    added code 3.4

---
+++ CSHP05D 3.4/CSHP05D 3.4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CSHP05D_3._4
+{
+    
+    class Ueberdeckt
+    {
+        string zeichenkette;
+
+
+        public void ZeichenketteAendern(string zeichenkette)
+        {
+            zeichenkette = zeichenkette + " geändert";
+        }
+
+        public string GetZeichenkette()
+        {
+            return zeichenkette;
+        }
+    }
+    
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ueberdeckt test = new Ueberdeckt();
+            test.ZeichenketteAendern("Test");
+
+            Console.WriteLine("Der geänderte Wert ist {0}", test.GetZeichenkette());
+
+            if (test.GetZeichenkette() == string.Empty)
+                Console.WriteLine("Die Zeichenkette ist leer");
+            else Console.WriteLine("Die Zeichenkette ist nicht leer");
+        }
+    }
+}
