aab5a0476958314dcbaa8627f5cd90cbf20e36a9
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-12-11 11:24:56 +0100

    Zeichenpositionierung

---
+++ Übungen CSHP/Übungen CSHP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Übungen_CSHP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\"\t");
+            Console.WriteLine("#\t\t#");
+            Console.WriteLine("#\t\t#");
+            Console.WriteLine("#\t\t#");
+            Console.WriteLine("#\t\t#");
+            Console.WriteLine("#\t\t#");
+            Console.WriteLine("\t\"\t");
+            Console.WriteLine("#\t\t#");
+            Console.WriteLine("#\t\t#");
+            Console.WriteLine("#\t\t#");
+            Console.WriteLine("#\t\t#");
+            Console.WriteLine("#\t\t#");
+            Console.WriteLine("\t\"\t");
+
+        }
+    }
+}
