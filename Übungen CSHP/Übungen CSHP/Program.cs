ec255e861e66954246d86bcd343b107ee014f2b7
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-12-15 11:33:21 +0100

    3.2

--- Übungen CSHP/Übungen CSHP/Program.cs
+++ Übungen CSHP/Übungen CSHP/Program.cs
@@ -7,21 +7,14 @@ namespace Übungen_CSHP
         static void Main(string[] args)
         {
 
-           bool prüfVariable = false;
+            for (int i = 1; i < 6; i++)
+            {
+                if(i == 5)
+                    Console.Write("{0}", i);
+                else
+                    Console.Write("{0},", i);
 
-            Console.WriteLine("Bitte geben Sie eine Jahreszahl ein, um zu überprüfen, ob es ein Schaltjahr ist.");
-           int jahresZahl = Convert.ToInt32(Console.ReadLine());
-
-            if ((jahresZahl % 4 == 0) && (jahresZahl % 100 != 0))
-                prüfVariable = true;
-
-            if ((jahresZahl % 4 == 0) && (jahresZahl % 100 == 0) && (jahresZahl % 400 == 0))
-                prüfVariable = true;
-
-            if (prüfVariable == true)
-                Console.WriteLine("Dieses Jahr ist ein Schaltjahr!");
-            else
-                Console.WriteLine("Dieses Jahr ist KEIN Schaltjahr!");
+            }
 
 
 
