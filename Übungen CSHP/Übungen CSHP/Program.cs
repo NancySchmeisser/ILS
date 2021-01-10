b170f2927008a149bb157deaea618dfd26cdb7c2
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-01-10 20:06:36 +0100

    Fernseher 1

--- Übungen CSHP/Übungen CSHP/Program.cs
+++ Übungen CSHP/Übungen CSHP/Program.cs
@@ -2,89 +2,44 @@
 
 namespace Übungen_CSHP
 {
+    class TV
+    {
+        public int lautstärke;
+        public int programm;
+        public bool eingeschaltet = false;
+
+       public void Anschalten()
+        {
+            eingeschaltet = true;
+            programm = 1;
+            lautstärke = 10;
+        }
+
+        public void ÄndereLautstärke()
+        {
+
+        }
+
+        public void ÄndereProgramm()
+        {
+
+        }
+
+        public void Ausschalten()
+        {
+            if (eingeschaltet == true)
+                Console.WriteLine();
+        }
+
+
+    }
+
     class Program
     {
-
-        public static double Addieren(double zahl1, double zahl2)
-        {
-            double ergebnis = zahl1 + zahl2;
-            return ergebnis;
-        }
-
-        public static double Subtrahieren(double zahl1, double zahl2)
-        {
-            double ergebnis = zahl1 - zahl2;
-            return ergebnis;
-        }
-
-        public static double Multiplizieren(double zahl1, double zahl2)
-        {
-            double ergebnis = zahl1 * zahl2;
-            return ergebnis;
-        }
-
-        public static double Dividieren(double zahl1, double zahl2)
-        {
-            double ergebnis = zahl1 % zahl2;
-            return ergebnis;
-        }
-
-        static double Potenz(double basis, double exponent)
-        {
-            double ergebnis = 1;
-
-            for (int i = 0; i < exponent; i++)
-            {
-                ergebnis = ergebnis * basis;
-            }
-            return ergebnis;
-
-        }
-
-
         static void Main(string[] args)
         {
-            string eingabe; 
-            double zahl1, zahl2;
+            
 
-            Console.WriteLine("Geben Sie die erste Zahl ein!");
-                zahl1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Geben Sie die zweite Zahl ein!");
-                zahl2 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Welche Rechenoperation möchten Sie durchführen? Addieren, Subtrahieren, Multiplizieren, Dividieren oder Potenzieren (^)");
-            eingabe = Console.ReadLine();
-            double ergebnis;
-
-
-            switch (eingabe) 
-            {
-                case "+":
-                    ergebnis = Addieren(zahl1, zahl2);
-                    break;
-
-                case "-":
-                    ergebnis = Subtrahieren(zahl1, zahl2);
-                    break;
-
-                case "*":
-                    ergebnis = Multiplizieren(zahl1, zahl2);
-                    break;
-
-                case "/":
-                    ergebnis = Dividieren(zahl1, zahl2);
-                    break;
-
-                case "^":
-                    ergebnis = Potenz(zahl1, zahl2);
-                    break;
-                default:
-                    Console.WriteLine("Geben Sie ein gültiges Zeichen ein! (+, -, *, /, p");
-                    ergebnis = 0;
-                    break;
-            }
-            Console.WriteLine("Das Ergebnis ist {0}", ergebnis);
         }
     }
 }
