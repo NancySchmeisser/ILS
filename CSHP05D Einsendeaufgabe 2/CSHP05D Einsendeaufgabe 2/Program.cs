b4607d60221682fed6b361ba8435fef6c1ec8d0f
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-01-11 14:25:13 +0100

    added new project

---
+++ CSHP05D Einsendeaufgabe 2/CSHP05D Einsendeaufgabe 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSHP05D_Einsendeaufgabe_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
