9918d989583766651b1b59071f4241cc3a184472
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-01-14 12:32:43 +0100

    added code 1.3

--- CSHP06D 1.1/CSHP06D 1.1/Program.cs
+++ CSHP06D 1.1/CSHP06D 1.1/Program.cs
@@ -5,18 +5,53 @@ namespace CSHP06D_1._1
 
     class Esel
     {
-        int sturheit;
+        public int sturheit;
         public int gewicht;
+
+
+        public void Arbeiten()
+        {
+            if (sturheit > 9 || gewicht < 10)
+                Console.WriteLine("Der Esel kann nicht arbeiten");
+            else
+            {
+                sturheit++;
+                gewicht--;
+                Console.WriteLine("Der Esel hat jetzt gearbeitet");
+            }
+        }
+
+        public void Fressen()
+        {
+            gewicht++;
+            if (sturheit > 0)
+                sturheit--;
+            Console.WriteLine("Der Esel hat jetzt gefressen");
+        }
+
+        public int Wiegen()
+        {
+            return gewicht;
+        }
+
+        public void Init()
+        {
+            gewicht = 15;
+            sturheit = 5;
+        }
     }
+
+
     class Program
     {
         static void Main(string[] args)
         {
             Esel eselchen = new Esel();
 
-            eselchen.gewicht = 10;
-
-            Console.WriteLine("Der Esel wiegt {0} Kilo", eselchen.gewicht);
+            eselchen.Init();
+            eselchen.Arbeiten();
+            eselchen.Fressen();
+            Console.WriteLine("Der Esel wiegt {0} Kilo.", eselchen.Wiegen());
             
         }
     }
