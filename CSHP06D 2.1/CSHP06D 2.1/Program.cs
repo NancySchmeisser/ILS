ccce5fa485798b493d26aaecebccc2d88f5f1c9f
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-01-15 10:09:52 +0100

    added code 2.1

---
+++ CSHP06D 2.1/CSHP06D 2.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CSHP06D_2._1
+{
+
+    class Sherry
+    {
+        int alter;
+        int liter;
+
+
+        public void Init(int alter, int liter)
+        {
+            this.alter = alter;
+            this.liter = liter;
+        }
+
+        public void Ansehen()
+        {
+            Console.WriteLine("Der Sherry ist {0} Jahre alt", alter);
+            Console.WriteLine("Die Flasche enthält {0} Liter", liter);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sherry flasche1 = new Sherry();
+
+            flasche1.Init(10, 1);
+
+            flasche1.Ansehen();
+
+        }
+    }
+}
