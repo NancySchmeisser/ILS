2d9b290d71a15c796780a45b378454e4d661802b
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-01-19 10:37:07 +0100

    added code 5.1

---
+++ CSHP06D 5.1/CSHP06D 5.1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CSHP06D_5._1
+{
+    class Baer
+    {
+        protected int gewicht;
+        protected int alter;
+    
+        public Baer(int gewicht, int alter)
+        {
+            this.gewicht = gewicht;
+            this.alter = alter;
+        }
+
+        public int GetGewicht()
+        {
+            return gewicht;
+        }
+
+
+        public int GetAlter()
+        {
+            return alter;
+        }
+
+        public void Ausgeben()
+        {
+            Console.WriteLine("Die Ausgabe erfolgt aus {0}", this.GetType());
+            Console.WriteLine("Der Bar ist {0} Jahre alt und wiegt {1} Kilo", alter, gewicht);
+        }
+    }
+
+    class ElternBaer: Baer
+    {
+        int anzahlKinder;
+
+        public ElternBaer(int gewicht, int alter, int anzahlKinder : base(gewicht, alter))
+        {
+            this.anzahlKinder = anzahlKinder;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
