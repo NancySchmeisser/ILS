a03b8cec270fe0292f0de863b55ed06ce1f92b47
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-01-19 14:27:19 +0100

    added code 6.1

---
+++ MeineKlassen/MeineKlassen/EinUndAusgabe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MeineKlassen
+{
+    public class EinUndAusgabe
+    {
+        static public string Eingabe()
+        {
+            string zeichenkette;
+            Console.WriteLine("Bitte geben Sie einen Wert ein: ");
+            zeichenkette = Console.ReadLine();
+            return zeichenkette;
+
+        }
+
+        static public void Ausgabe(string wert)
+        {
+            Console.WriteLine("Ihre Eingabe war: {0}", wert);
+        }
+    }
+}
