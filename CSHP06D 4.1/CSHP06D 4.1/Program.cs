3d1e3b839b0841f8642ab674b18ca788f099aa72
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-01-19 09:44:39 +0100

    added code 4.5

--- CSHP06D 4.1/CSHP06D 4.1/Program.cs
+++ CSHP06D 4.1/CSHP06D 4.1/Program.cs
@@ -4,13 +4,13 @@ namespace CSHP06D_4._1
 {
     class Baer
     {
-        int gewicht;
+        protected int gewicht;
         int alter;
 
-        public Baer()
+        public Baer(int gewicht, int alter)
         {
-            gewicht = 100;
-            alter = 5;
+            this.gewicht = gewicht;
+            this.alter = alter;
         }
 
         public int GetGewicht()
@@ -22,20 +22,27 @@ namespace CSHP06D_4._1
         {
             return alter;
         }
+
+   
     }
 
     class ElternBaer : Baer
     {
         int anzahlKinder;
 
-        public ElternBaer()
+        public ElternBaer(int gewicht, int alter, int anzahlKinder): base(gewicht, alter) 
         {
-            anzahlKinder = 2;
+            this.anzahlKinder = anzahlKinder;
         }
 
         public int GetAnzahlKinder()
         {
             return anzahlKinder;
+        }
+
+        public void Schock()
+        {
+            gewicht--;
         }
     }
 
@@ -43,12 +50,15 @@ namespace CSHP06D_4._1
     {
         static void Main(string[] args)
         {
-            Baer alterBär = new Baer();
+            Baer alterBär = new Baer(200,3);
 
-            ElternBaer papaBaer = new ElternBaer();
+            ElternBaer papaBaer = new ElternBaer(500,5,20);
 
             Console.WriteLine("Der alte Bär ist {0} Jahre alt und wiegt {1} Kilo", alterBär.GetAlter(), alterBär.GetGewicht());
             Console.WriteLine("Der Papa-Bär ist {0} Jahre alt und wiegt {1} Kilo und hat {2} Kinder", papaBaer.GetAlter(), papaBaer.GetGewicht(), papaBaer.GetAnzahlKinder());
+            papaBaer.Schock();
+            Console.WriteLine("Der Papa-Bär ist {0} Jahre alt und wiegt {1} Kilo und hat {2} Kinder", papaBaer.GetAlter(), papaBaer.GetGewicht(), papaBaer.GetAnzahlKinder());
+
         }
     }
 }
