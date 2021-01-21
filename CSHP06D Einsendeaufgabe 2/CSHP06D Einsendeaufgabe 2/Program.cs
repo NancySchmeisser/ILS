3e6d7a3c9a6e5108f95195f8ef1b3cd50fb24713
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-01-21 11:56:15 +0100

    added testcode

---
+++ CSHP06D Einsendeaufgabe 2/CSHP06D Einsendeaufgabe 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSHP06D_Einsendeaufgabe_2
+{
+
+    class Katze
+    {
+        int groesse;
+
+        public Katze(int groesse)
+        {
+            this.groesse = groesse;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
