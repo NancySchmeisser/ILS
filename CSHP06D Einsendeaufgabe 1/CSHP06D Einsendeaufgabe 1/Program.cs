dd9958511d0d7ae4ca65907a6b1e8fe7a0114c11
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-01-21 10:10:17 +0100

    added  testcode

---
+++ CSHP06D Einsendeaufgabe 1/CSHP06D Einsendeaufgabe 1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CSHP06D_Einsendeaufgabe_1
+{
+
+    class Hund
+    {
+        int alter = 5;
+        int gewicht = 20;
+
+        public int GetAlter()
+        {
+            return alter;
+        }
+
+        public int GetGewicht()
+        {
+            return gewicht;
+        }
+    }
+
+    class KleinerHund : Hund
+    {
+        int größe = 10;
+
+        public int GetGroeße()
+        {
+            return größe;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KleinerHund Struppi = new KleinerHund();
+
+            Console.WriteLine("Struppi ist {0} Jahre alt, wiegt {1} Kilo und ist {2} Zentimeter groß", Struppi.GetAlter(), Struppi.GetGewicht(), Struppi.GetGroeße());
+        }
+    }
+}
