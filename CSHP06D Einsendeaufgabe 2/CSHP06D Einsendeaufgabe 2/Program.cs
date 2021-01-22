9692f1e469bfa851141d3a5f33ef6b149742356e
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-01-22 09:08:31 +0100

    added testcode

--- CSHP06D Einsendeaufgabe 2/CSHP06D Einsendeaufgabe 2/Program.cs
+++ CSHP06D Einsendeaufgabe 2/CSHP06D Einsendeaufgabe 2/Program.cs
@@ -16,7 +16,10 @@ namespace CSHP06D_Einsendeaufgabe_2
     {
         static void Main(string[] args)
         {
-            
+     //       Katze kleineKatze = new Katze();
         }
     }
 }
+
+//Die Ausführung funktioniert nicht. („There is no argument given that corresponds tot he required formal parameter „groesse“ of Katze.Katze(int)“
+//In der Klasse Katze wurde ein eigener Konstruktor erstellt, damit wird kein Standardkonstruktor angelegt.. In der Instanz der Klasse Katze wird der nicht mehr vorhandene Standardkonstruktor aufgerufen, was dann nicht funktionert.
