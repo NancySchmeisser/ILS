fe1e89b25155d0561b89dbdea0c95f0a20b031d3
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-01-25 11:09:57 +0100

    CSHP06D Einsendeaufgabe 4

--- CSHP06D 4.1/CSHP06D 4.1/Program.cs
+++ CSHP06D 4.1/CSHP06D 4.1/Program.cs
@@ -5,7 +5,7 @@ namespace CSHP06D_4._1
     class Baer
     {
         protected int gewicht;
-        int alter;
+        protected int alter;
 
         public Baer(int gewicht, int alter)
         {
@@ -21,6 +21,13 @@ namespace CSHP06D_4._1
         public int GetAlter()
         {
             return alter;
+        }
+
+        public void Ausgeben()
+        {
+            Console.WriteLine("Die Ausgabe erfolgt aus {0}", this.GetType());
+            Console.WriteLine("Der alte Bär ist {0} Jahre alt und wiegt {1} Kilo", alter, gewicht);
+
         }
 
    
@@ -44,20 +51,36 @@ namespace CSHP06D_4._1
         {
             gewicht--;
         }
+
+        public void Ausgeben()
+        {
+            Console.WriteLine("Die Ausgabe erfolgt aus {0}", this.GetType());
+            Console.WriteLine("Der alte Bär ist {0} Jahre alt und wiegt {1} Kilo und hat {2} Kinder", alter, gewicht, anzahlKinder);
+
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Baer alterBär = new Baer(200,3);
+            /* Baer alterBär = new Baer(200,3);
 
-            ElternBaer papaBaer = new ElternBaer(500,5,20);
 
-            Console.WriteLine("Der alte Bär ist {0} Jahre alt und wiegt {1} Kilo", alterBär.GetAlter(), alterBär.GetGewicht());
-            Console.WriteLine("Der Papa-Bär ist {0} Jahre alt und wiegt {1} Kilo und hat {2} Kinder", papaBaer.GetAlter(), papaBaer.GetGewicht(), papaBaer.GetAnzahlKinder());
-            papaBaer.Schock();
-            Console.WriteLine("Der Papa-Bär ist {0} Jahre alt und wiegt {1} Kilo und hat {2} Kinder", papaBaer.GetAlter(), papaBaer.GetGewicht(), papaBaer.GetAnzahlKinder());
+             ElternBaer papaBaer = new ElternBaer(500,5,20);
+
+             Console.WriteLine("Der alte Bär ist {0} Jahre alt und wiegt {1} Kilo", alterBär.GetAlter(), alterBär.GetGewicht());
+             Console.WriteLine("Der Papa-Bär ist {0} Jahre alt und wiegt {1} Kilo und hat {2} Kinder", papaBaer.GetAlter(), papaBaer.GetGewicht(), papaBaer.GetAnzahlKinder());
+             papaBaer.Schock();
+             Console.WriteLine("Der Papa-Bär ist {0} Jahre alt und wiegt {1} Kilo und hat {2} Kinder", papaBaer.GetAlter(), papaBaer.GetGewicht(), papaBaer.GetAnzahlKinder());*/
+
+
+            Baer[] Baerenliste = new Baer[2];
+            Baerenliste[0] = new Baer(200, 3);
+            Baerenliste[1] = new ElternBaer(500, 5, 20);
+            foreach (Baer testBaer in Baerenliste)
+                testBaer.Ausgeben();
+
 
         }
     }
