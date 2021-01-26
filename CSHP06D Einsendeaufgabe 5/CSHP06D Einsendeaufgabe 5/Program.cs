24c959099705b04c9c85b0fc860c4d5f89b573ce
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-01-26 09:24:08 +0100

    added testcode

---
+++ CSHP06D Einsendeaufgabe 5/CSHP06D Einsendeaufgabe 5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSHP06D_Einsendeaufgabe_5
+{
+    class Haustiere
+    {
+        int gewicht;
+        int alter;
+        public Haustiere(int gewicht, int alter)
+        {
+            this.gewicht = gewicht;
+            this.alter = alter;
+        }
+    }
+
+    class Vogel : Haustiere
+    {
+        int flügelLänge;
+        public Vogel(int gewicht, int alter, int flügelLänge) : base (gewicht, alter)
+        {
+            this.flügelLänge = flügelLänge;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
