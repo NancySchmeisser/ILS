300f09984f28cc66293c5e68259131df41a056d2
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-01-29 08:11:12 +0100

    added testcode

---
+++ CSHP07D 3/CSHP07D 3/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace CSHP07D_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonKopieren = new System.Windows.Forms.Button();
+            this.buttonBeenden = new System.Windows.Forms.Button();
+            this.labelQuelle = new System.Windows.Forms.Label();
+            this.labelZiel = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // buttonKopieren
+            // 
+            this.buttonKopieren.Location = new System.Drawing.Point(12, 125);
+            this.buttonKopieren.Name = "buttonKopieren";
+            this.buttonKopieren.Size = new System.Drawing.Size(121, 23);
+            this.buttonKopieren.TabIndex = 0;
+            this.buttonKopieren.Text = "Kopieren";
+            this.buttonKopieren.UseVisualStyleBackColor = true;
+            this.buttonKopieren.Click += new System.EventHandler(this.buttonKopieren_Click);
+            // 
+            // buttonBeenden
+            // 
+            this.buttonBeenden.Location = new System.Drawing.Point(171, 125);
+            this.buttonBeenden.Name = "buttonBeenden";
+            this.buttonBeenden.Size = new System.Drawing.Size(121, 23);
+            this.buttonBeenden.TabIndex = 1;
+            this.buttonBeenden.Text = "Beenden";
+            this.buttonBeenden.UseVisualStyleBackColor = true;
+            this.buttonBeenden.Click += new System.EventHandler(this.buttonBeenden_Click);
+            // 
+            // labelQuelle
+            // 
+            this.labelQuelle.AutoSize = true;
+            this.labelQuelle.Location = new System.Drawing.Point(12, 13);
+            this.labelQuelle.Name = "labelQuelle";
+            this.labelQuelle.Size = new System.Drawing.Size(37, 13);
+            this.labelQuelle.TabIndex = 2;
+            this.labelQuelle.Text = "Quelle";
+            this.labelQuelle.DoubleClick += new System.EventHandler(this.buttonKopieren_Click);
+            // 
+            // labelZiel
+            // 
+            this.labelZiel.AutoSize = true;
+            this.labelZiel.Location = new System.Drawing.Point(171, 13);
+            this.labelZiel.Name = "labelZiel";
+            this.labelZiel.Size = new System.Drawing.Size(24, 13);
+            this.labelZiel.TabIndex = 3;
+            this.labelZiel.Text = "Ziel";
+            this.labelZiel.DoubleClick += new System.EventHandler(this.buttonKopieren_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Kaffee",
+            "Gurken",
+            "Banana",
+            "Schere"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Lama",
+            "Maus",
+            "Milch"});
+            this.listBox1.Location = new System.Drawing.Point(171, 47);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(121, 56);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(12, 91);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(79, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Einblenden";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 175);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.labelZiel);
+            this.Controls.Add(this.labelQuelle);
+            this.Controls.Add(this.buttonBeenden);
+            this.Controls.Add(this.buttonKopieren);
+            this.Name = "Form1";
+            this.Text = "Spielereien";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonKopieren;
+        private System.Windows.Forms.Button buttonBeenden;
+        private System.Windows.Forms.Label labelQuelle;
+        private System.Windows.Forms.Label labelZiel;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ CSHP07D 3/CSHP07D 3/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSHP07D_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonKopieren_Click(object sender, EventArgs e)
+        {
+            string tempKette;
+            //den Text aus dem Ziel in Sicherheit bringen
+            tempKette = labelZiel.Text;
+            //den Text aus der Quelle in das Ziel kopieren
+            labelZiel.Text = labelQuelle.Text;
+            //den gesicherten Text in die Quelle kopieren
+            labelQuelle.Text = tempKette;
+        }
+
+        private void buttonBeenden_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            labelQuelle.Text = comboBox1.SelectedItem.ToString();
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            labelZiel.Text = listBox1.SelectedItem.ToString();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+
+                listBox1.Visible = true;
+                comboBox1.Visible = true;
+            }
+            else
+            {
+                listBox1.Visible = false;
+                comboBox1.Visible = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex = 0;
+            comboBox1.SelectedIndex = 0;
+
+            labelZiel.Text = "Ziel";
+            labelQuelle.Text = "Quelle";
+        }
+    }
+}
