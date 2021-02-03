658d0535e45415060f7ff5e084d051db1a45bec0
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-02-03 12:56:39 +0100

    Kapitel 1

---
+++ Bildbetrachter/Bildbetrachter/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Bildbetrachter
+{
+    partial class Bildbetrachter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageEinzel = new System.Windows.Forms.TabPage();
+            this.tabPageSchau = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPageEinzel);
+            this.tabControl1.Controls.Add(this.tabPageSchau);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 426);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPageEinzel
+            // 
+            this.tabPageEinzel.Location = new System.Drawing.Point(4, 22);
+            this.tabPageEinzel.Name = "tabPageEinzel";
+            this.tabPageEinzel.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageEinzel.Size = new System.Drawing.Size(768, 400);
+            this.tabPageEinzel.TabIndex = 0;
+            this.tabPageEinzel.Text = "Einzelbild";
+            this.tabPageEinzel.UseVisualStyleBackColor = true;
+            // 
+            // tabPageSchau
+            // 
+            this.tabPageSchau.Location = new System.Drawing.Point(4, 22);
+            this.tabPageSchau.Name = "tabPageSchau";
+            this.tabPageSchau.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageSchau.Size = new System.Drawing.Size(768, 400);
+            this.tabPageSchau.TabIndex = 1;
+            this.tabPageSchau.Text = "Bilderschau";
+            this.tabPageSchau.UseVisualStyleBackColor = true;
+            // 
+            // Bildbetrachter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Bildbetrachter";
+            this.Text = "Bildbetrachter";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageEinzel;
+        private System.Windows.Forms.TabPage tabPageSchau;
+    }
+}
+
