942555813dd17f38df509f6fca87bbd0bae2fd80
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-02-03 10:40:22 +0100

    Einsendeaufgabe 4

---
+++ CSHP07D Einsendeaufgabe 4/CSHP07D Einsendeaufgabe 4/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CSHP07D_Einsendeaufgabe_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(141, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " Label";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(121, 334);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "LabelHop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 562);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CSHP07D Einsendeaufgabe 4/CSHP07D Einsendeaufgabe 4/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSHP07D_Einsendeaufgabe_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            label1.Top = 10;
+
+           do
+            {
+                label1.Top+=30;
+                Thread.Sleep(500);
+                counter++;
+            } while (counter < 10);
+            
+        }
+    }
+}
