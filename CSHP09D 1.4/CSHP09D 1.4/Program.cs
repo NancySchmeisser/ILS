4cbbd0d6bf3feb3500bb36a3cd49940c035bc1f3
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-03-09 11:16:01 +0100

    added code 1.4

---
+++ CSHP09D 1.4/CSHP09D 1.4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSHP09D_1._4
+{
+    class Program
+    {
+        static int Subtraktion(int wert1, int wert2)
+        {
+            return (wert1 - wert2);
+        }
+        static void Main(string[] args)
+        {
+            int zahl;
+            zahl = Subtraktion(10, 20);
+            Console.WriteLine("Zahl hat den Wert {0}.", zahl);
+        }
+    }
+}
