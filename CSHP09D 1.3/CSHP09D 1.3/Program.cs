20e2324c368d1ef708d41a9fec663efd34e080ab
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-03-09 11:10:08 +0100

    added code 1.3

---
+++ CSHP09D 1.3/CSHP09D 1.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSHP09D_1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int zahl;
+            Console.WriteLine("Bitte geben Sie eine ganze Zahl ein: ");
+            zahl = Convert.ToInt32(Console.ReadLine());
+
+            if (zahl == 5)
+                zahl = zahl + 10;
+            if (zahl <= 5)
+                zahl = zahl + 20;
+            if (zahl > 5)
+                zahl = zahl + 30;
+            Console.WriteLine("Zahl hat den Wert {0}.", zahl);
+
+        }
+    }
+}
