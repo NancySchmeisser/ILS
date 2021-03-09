d8b3d60f0c02b1f7f71ff88a86175018f48d687f
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-03-09 10:55:52 +0100

    added code 1.1

---
+++ CSHP09D/CSHP09D/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSHP09D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int zaehler = 1;
+            while (zaehler < 10)
+                Console.WriteLine("Schelifendurchlauf {0}", zaehler);
+            
+        }
+    }
+}
