8e7c597fd134537fa4f83e396bda7ccf2778366e
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-03-18 10:39:23 +0100

    added code 5.3

---
+++ CSHP09D 5.3/CSHP09D 5.3/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSHP09D_5._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            int ergebnis, zahl2, zahl1 = 10;
+
+            try
+            {
+                zahl2 = Convert.ToInt32(textBox1.Text);
+                ergebnis = zahl1 / zahl2;
+                MessageBox.Show("Das Ergebnis ist " + ergebnis, "Hurra");
+            }
+
+            catch
+            {
+                MessageBox.Show("Da ist etwas schief gegangen", "oh nein");
+            }
+        }
+    }
+}
