abe7f78ab7df06c16f153e0e90eadd932eff3ed1
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-06-02 13:24:54 +0200

    add code 1.8

--- CSHP10D/CSHP10D/Form1.Designer.cs
+++ CSHP10D/CSHP10D/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.Schließen = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Schließen
@@ -42,11 +43,22 @@
             this.Schließen.UseVisualStyleBackColor = true;
             this.Schließen.Click += new System.EventHandler(this.Schließen_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.Schließen);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -58,6 +70,7 @@
         #endregion
 
         private System.Windows.Forms.Button Schließen;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- CSHP10D/CSHP10D/Form1.cs
+++ CSHP10D/CSHP10D/Form1.cs
@@ -24,25 +24,34 @@ namespace CSHP10D
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            //einen schwarzen Stift erzeugen
-            Pen stift = new Pen(Color.Black);
-            //eine Variable vom Typ Rectangle
-            Rectangle bereich;
-            //den Client-Bereich beschaffen
-            bereich = ClientRectangle;
-            //in einer Schleife anpassen
-            for (int durchlauf = 0; durchlauf < 22; durchlauf++)
-            {
-                bereich.Width = bereich.Width - 10;
-                bereich.Height = bereich.Height - 10;
-                bereich.Location = new Point(bereich.Location.X + 5, bereich.Location.Y + 5);
-                //ein Rechteck zeichnen
-                e.Graphics.DrawRectangle(stift, bereich);
-                //einen Moment warten
-                System.Threading.Thread.Sleep(100);
-                //den Zeichenbereich löschen
-                e.Graphics.Clear(BackColor);
+           
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        // eine Variable vom Typ Graphics
+        Graphics zeichenflaeche;
+        //die Variable auf die Zeichenfläche des Formulars setzen
+        zeichenflaeche = this.CreateGraphics();
+        //einen schwarzen Stift erzeugen
+        Pen stift = new Pen(Color.Black);
+        //eine Variable vom Typ Rectangle
+        Rectangle bereich;
+        //den Client-Bereich beschaffen
+        bereich = ClientRectangle;
+        //in einer Schleife anpassen
+        for (int durchlauf = 0; durchlauf < 22; durchlauf++)
+        {
+            bereich.Width = bereich.Width - 10;
+            bereich.Height = bereich.Height - 10;
+            bereich.Location = new Point(bereich.Location.X + 5, bereich.Location.Y + 5);
+            //ein Rechteck zeichnen
+            zeichenflaeche.DrawRectangle(stift, bereich);
+            //einen Moment warten
+            System.Threading.Thread.Sleep(100);
+            //den Zeichenbereich löschen
+            zeichenflaeche.Clear(BackColor);
         }
     }
 }
