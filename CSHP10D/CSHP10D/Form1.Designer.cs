27cbefbe0b2c05fe3bcd43f7f94271f272c88a96
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-06-07 10:39:38 +0200

    add code 1.8

--- CSHP10D/CSHP10D/Form1.Designer.cs
+++ CSHP10D/CSHP10D/Form1.Designer.cs
@@ -28,41 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Schließen = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Schließen
+            // button2
             // 
-            this.Schließen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.Schließen.Location = new System.Drawing.Point(397, 12);
-            this.Schließen.Name = "Schließen";
-            this.Schließen.Size = new System.Drawing.Size(75, 23);
-            this.Schließen.TabIndex = 0;
-            this.Schließen.Text = "Schließen";
-            this.Schließen.UseVisualStyleBackColor = true;
-            this.Schließen.Click += new System.EventHandler(this.Schließen_Click);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(80, 40);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button2.Location = new System.Drawing.Point(197, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Schließen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Schließen_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 461);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.Schließen);
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button2);
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
 
         }
@@ -71,6 +54,7 @@
 
         private System.Windows.Forms.Button Schließen;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- CSHP10D/CSHP10D/Form1.cs
+++ CSHP10D/CSHP10D/Form1.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace CSHP10D
+﻿namespace CSHP10D
 {
+    using System;
+    using System.Drawing;
+    using System.Windows.Forms;
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -24,34 +18,34 @@ namespace CSHP10D
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-           
-            }
         }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
-        // eine Variable vom Typ Graphics
-        Graphics zeichenflaeche;
-        //die Variable auf die Zeichenfläche des Formulars setzen
-        zeichenflaeche = this.CreateGraphics();
-        //einen schwarzen Stift erzeugen
-        Pen stift = new Pen(Color.Black);
-        //eine Variable vom Typ Rectangle
-        Rectangle bereich;
-        //den Client-Bereich beschaffen
-        bereich = ClientRectangle;
-        //in einer Schleife anpassen
-        for (int durchlauf = 0; durchlauf < 22; durchlauf++)
-        {
-            bereich.Width = bereich.Width - 10;
-            bereich.Height = bereich.Height - 10;
-            bereich.Location = new Point(bereich.Location.X + 5, bereich.Location.Y + 5);
-            //ein Rechteck zeichnen
-            zeichenflaeche.DrawRectangle(stift, bereich);
-            //einen Moment warten
-            System.Threading.Thread.Sleep(100);
-            //den Zeichenbereich löschen
-            zeichenflaeche.Clear(BackColor);
+            // eine Variable vom Typ Graphics
+            Graphics zeichenflaeche;
+            //die Variable auf die Zeichenfläche des Formulars setzen
+            zeichenflaeche = this.CreateGraphics();
+            //einen schwarzen Stift erzeugen
+            Pen stift = new Pen(Color.Black);
+            //eine Variable vom Typ Rectangle
+            Rectangle bereich;
+            //den Client-Bereich beschaffen
+            bereich = ClientRectangle;
+            //in einer Schleife anpassen
+            for (int durchlauf = 0; durchlauf < 22; durchlauf++)
+            {
+                bereich.Width = bereich.Width - 10;
+                bereich.Height = bereich.Height - 10;
+                bereich.Location = new Point(bereich.Location.X + 5, bereich.Location.Y + 5);
+                //ein Rechteck zeichnen
+                zeichenflaeche.DrawRectangle(stift, bereich);
+                //einen Moment warten
+                System.Threading.Thread.Sleep(100);
+                //den Zeichenbereich löschen
+                zeichenflaeche.Clear(BackColor);
+            }
         }
     }
 }
