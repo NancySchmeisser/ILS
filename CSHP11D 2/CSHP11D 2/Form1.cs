fb14fed72b1122f1464ac13970a23a33da87b406
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-08-04 11:00:36 +0200

    add code 2.1

--- CSHP11D 2/CSHP11D 2/Form1.cs
+++ CSHP11D 2/CSHP11D 2/Form1.cs
@@ -12,6 +12,34 @@ namespace CSHP11D_2
 {
     public partial class Form1 : Form
     {
+        //die Felder
+        //eine Struktur für die Richtung des Balls
+
+        struct Spielball
+        {
+            //wenn die Richtung true ist, geht es nach oben
+            //bzw nach rechts
+            //sonst nach unten bzw. nach links
+            public bool richtungX;
+            public bool richtungY;
+            //für die Veränderung des Bewegungswinkels
+            public int winkel;
+        }
+
+        //für die Zeichenfläche
+        Graphics zeichenfläche;
+        //für das Spielfeld
+        Rectangle spielfeldGroesse;
+        int spielfeldMaxX, spielfeldMaxY, spielfeldMinX, spielfeldMinY;
+        int spielfeldLininenbreite;
+        //für den Schläger
+
+        int schlaegerGroesse;
+        //für den Ball
+        Spielball ballPosition;
+        //zum Zeichnen
+        SolidBrush pinsel;
+
         public Form1()
         {
             InitializeComponent();
