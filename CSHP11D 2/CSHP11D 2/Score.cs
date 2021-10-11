dc257cfbd7e374bc53c6f5e8f0334ce253780b81
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-10-11 08:42:24 +0200

    add code 4.1

---
+++ CSHP11D 2/CSHP11D 2/Score.cs
@@ -0,0 +1,23 @@
+﻿namespace CSHP11D_2
+{
+    internal class Score
+    {
+        internal int punkte;
+
+        public Score()
+        {
+            punkte = 0;
+        }
+
+        public int VeraenderePunkte(int anzahl)
+        {
+            punkte = punkte + anzahl;
+            return punkte;
+        }
+
+        public void LoeschePunkte()
+        {
+            punkte = 0;
+        }
+    }
+}
