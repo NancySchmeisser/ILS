dc5a74376d4129e2e47d9eff0f781260fef8b36d
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-10-25 09:50:56 +0200

    add method ButtonLesen_Click

--- CSHP12D/CSHP12D/Form1.Designer.cs
+++ CSHP12D/CSHP12D/Form1.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.ButtonLesen = new System.Windows.Forms.Button();
+            this.ButtonSchreiben = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
@@ -43,23 +43,24 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
-            // button1
+            // ButtonLesen
             // 
-            this.button1.Location = new System.Drawing.Point(52, 116);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.ButtonLesen.Location = new System.Drawing.Point(52, 116);
+            this.ButtonLesen.Name = "ButtonLesen";
+            this.ButtonLesen.Size = new System.Drawing.Size(75, 23);
+            this.ButtonLesen.TabIndex = 1;
+            this.ButtonLesen.Text = "Lesen";
+            this.ButtonLesen.UseVisualStyleBackColor = true;
+            this.ButtonLesen.Click += new System.EventHandler(this.ButtonLesen_Click);
             // 
-            // button2
+            // ButtonSchreiben
             // 
-            this.button2.Location = new System.Drawing.Point(52, 157);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.ButtonSchreiben.Location = new System.Drawing.Point(52, 157);
+            this.ButtonSchreiben.Name = "ButtonSchreiben";
+            this.ButtonSchreiben.Size = new System.Drawing.Size(75, 23);
+            this.ButtonSchreiben.TabIndex = 2;
+            this.ButtonSchreiben.Text = "Schreiben";
+            this.ButtonSchreiben.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
@@ -74,8 +75,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(223, 294);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ButtonSchreiben);
+            this.Controls.Add(this.ButtonLesen);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -87,8 +88,8 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button ButtonLesen;
+        private System.Windows.Forms.Button ButtonSchreiben;
         private System.Windows.Forms.TextBox textBox1;
     }
 }
--- CSHP12D/CSHP12D/Form1.cs
+++ CSHP12D/CSHP12D/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,25 @@ namespace CSHP12D
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void ButtonLesen_Click(object sender, EventArgs e)
+        {
+            //den Schlüssel
+            //HKEY_CURRENT_USER\Software\RegistryDemo öffnen
+            using (RegistryKey regSchluessel = Registry.CurrentUser.OpenSubKey("Software\\RegistryDemo"))
+            {
+                //wenn der Schlüssel nicht vorhanden ist, eine
+                //Meldung ausgeben
+                if (regSchluessel == null)
+                {
+                    MessageBox.Show("Der Schlüssel konnte nicht geöffnet werden!");
+                    label1.Text = "Nicht vorhanden";
+                }
+                else
+                    //sonst den Eintrag lesen und label1 zuweisen
+                    label1.Text = Convert.ToString(regSchluessel.GetValue("Eintrag"));
+            }
         }
     }
 }
