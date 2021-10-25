371221edac29e03eaee3769ad83c002a110aad9e
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-10-25 10:09:29 +0200

    add method Form1_Shown

--- CSHP12D/CSHP12D/Form1.cs
+++ CSHP12D/CSHP12D/Form1.cs
@@ -1,16 +1,9 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace CSHP12D
+﻿namespace CSHP12D
 {
+    using Microsoft.Win32;
+    using System;
+    using System.Windows.Forms;
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -50,7 +43,7 @@ namespace CSHP12D
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
-            //beim Schlißen speichern wir die Position und die
+            //beim Schließen speichern wir die Position und die
             //Größe in der Registrierung
             //den Schlüssel HKEY_CURRENT_USER\Software\RegistryDemo
             //anlegen bzw. öffnen
@@ -61,6 +54,27 @@ namespace CSHP12D
                 regSchluessel.SetValue("Left", this.Left);
                 regSchluessel.SetValue("Width", this.Width);
                 regSchluessel.SetValue("Height", this.Height);
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            //beim Anzeigen lesen wir die Daten wieder zurück
+            //den Schlüssel HKEY_CURRENT_USER\Software\RegistryDemo öffnen
+            using (RegistryKey regSchluessel = Registry.CurrentUser.OpenSubKey("Software\\RegistryDemo"))
+            {
+                //wenn der Schlüssel nicht vorhanden ist, Meldung ausgeben
+                if (regSchluessel == null)
+                    MessageBox.Show("Der Schlüssel konnte nicht geöffnet werden!");
+                else
+                {
+                    //sonst die Einsträge lesen und zuweisen
+                    this.Top = Convert.ToInt32(regSchluessel.GetValue("Top"));
+                    this.Left = Convert.ToInt32(regSchluessel.GetValue("Left"));
+                    this.Width = Convert.ToInt32(regSchluessel.GetValue("Width"));
+                    this.Height = Convert.ToInt32(regSchluessel.GetValue("Height"));
+
+                }
             }
         }
     }
