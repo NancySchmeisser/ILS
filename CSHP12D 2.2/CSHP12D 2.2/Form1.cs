e2b00496f8e1a4c9937b6979678244c0efc9aec0
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-10-28 09:00:10 +0200

    add code 2.1

--- CSHP12D 2.2/CSHP12D 2.2/Form1.cs
+++ CSHP12D 2.2/CSHP12D 2.2/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace CSHP12D_2._2
 {
@@ -15,6 +16,18 @@ namespace CSHP12D_2._2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //eine neue Instanz für XMLReader über Create() erzeugen
+            XmlReader xmlLesen = XmlReader.Create("c:\\test\\test.xml");
+            //solange Daten gelesen werden können
+            while (xmlLesen.Read())
+                //den Wert in das Listenfeld schreiben
+                listBox1.Items.Add(xmlLesen.Value);
+            //die Datei wieder schließen
+            xmlLesen.Close();
         }
     }
 }
