c5e06167c2e5996512430e6caab51326fd740d65
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-11-04 10:09:05 +0100

    format

--- CSHP12D/Einsendeaufgabe/CSHP12D Einsendeaufgabe 1/CSHP12D 1.5/Form1.Designer.cs
+++ CSHP12D/Einsendeaufgabe/CSHP12D Einsendeaufgabe 1/CSHP12D 1.5/Form1.Designer.cs
@@ -32,7 +32,7 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.listBox2 = new System.Windows.Forms.ListBox();
             this.listBox3 = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.ButtonBeenden = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ButtonEinlesen
@@ -73,22 +73,22 @@
             this.listBox3.Sorted = true;
             this.listBox3.TabIndex = 3;
             // 
-            // button1
+            // ButtonBeenden
             // 
-            this.button1.Location = new System.Drawing.Point(310, 186);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(127, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Beenden";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.ButtonBeenden.Location = new System.Drawing.Point(310, 186);
+            this.ButtonBeenden.Name = "ButtonBeenden";
+            this.ButtonBeenden.Size = new System.Drawing.Size(127, 23);
+            this.ButtonBeenden.TabIndex = 4;
+            this.ButtonBeenden.Text = "Beenden";
+            this.ButtonBeenden.UseVisualStyleBackColor = true;
+            this.ButtonBeenden.Click += new System.EventHandler(this.ButtonBeenden_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(483, 247);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ButtonBeenden);
             this.Controls.Add(this.listBox3);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
@@ -105,7 +105,7 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.ListBox listBox2;
         private System.Windows.Forms.ListBox listBox3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button ButtonBeenden;
     }
 }
 
--- CSHP12D/Einsendeaufgabe/CSHP12D Einsendeaufgabe 1/CSHP12D 1.5/Form1.cs
+++ CSHP12D/Einsendeaufgabe/CSHP12D Einsendeaufgabe 1/CSHP12D 1.5/Form1.cs
@@ -2,6 +2,13 @@
 {
     using Microsoft.Win32;
     using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
     using System.Windows.Forms;
 
     public partial class Form1 : Form
@@ -66,7 +73,7 @@
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void ButtonBeenden_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
