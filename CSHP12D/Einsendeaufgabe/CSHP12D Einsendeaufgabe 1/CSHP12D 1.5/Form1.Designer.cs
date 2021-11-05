da60a3d50da1c1193590940b96e1551fef211f72
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-11-05 13:31:26 +0100

    change design

--- CSHP12D/Einsendeaufgabe/CSHP12D Einsendeaufgabe 1/CSHP12D 1.5/Form1.Designer.cs
+++ CSHP12D/Einsendeaufgabe/CSHP12D Einsendeaufgabe 1/CSHP12D 1.5/Form1.Designer.cs
@@ -37,9 +37,9 @@
             // 
             // ButtonEinlesen
             // 
-            this.ButtonEinlesen.Location = new System.Drawing.Point(20, 186);
+            this.ButtonEinlesen.Location = new System.Drawing.Point(20, 309);
             this.ButtonEinlesen.Name = "ButtonEinlesen";
-            this.ButtonEinlesen.Size = new System.Drawing.Size(127, 23);
+            this.ButtonEinlesen.Size = new System.Drawing.Size(127, 32);
             this.ButtonEinlesen.TabIndex = 0;
             this.ButtonEinlesen.Text = "Einlesen";
             this.ButtonEinlesen.UseVisualStyleBackColor = true;
@@ -50,7 +50,7 @@
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(20, 28);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(127, 95);
+            this.listBox1.Size = new System.Drawing.Size(127, 251);
             this.listBox1.Sorted = true;
             this.listBox1.TabIndex = 1;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
@@ -60,7 +60,7 @@
             this.listBox2.FormattingEnabled = true;
             this.listBox2.Location = new System.Drawing.Point(162, 28);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(127, 95);
+            this.listBox2.Size = new System.Drawing.Size(127, 251);
             this.listBox2.Sorted = true;
             this.listBox2.TabIndex = 2;
             this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
@@ -70,15 +70,15 @@
             this.listBox3.FormattingEnabled = true;
             this.listBox3.Location = new System.Drawing.Point(310, 28);
             this.listBox3.Name = "listBox3";
-            this.listBox3.Size = new System.Drawing.Size(127, 95);
+            this.listBox3.Size = new System.Drawing.Size(224, 251);
             this.listBox3.Sorted = true;
             this.listBox3.TabIndex = 3;
             // 
             // ButtonBeenden
             // 
-            this.ButtonBeenden.Location = new System.Drawing.Point(310, 186);
+            this.ButtonBeenden.Location = new System.Drawing.Point(407, 309);
             this.ButtonBeenden.Name = "ButtonBeenden";
-            this.ButtonBeenden.Size = new System.Drawing.Size(127, 23);
+            this.ButtonBeenden.Size = new System.Drawing.Size(127, 32);
             this.ButtonBeenden.TabIndex = 4;
             this.ButtonBeenden.Text = "Beenden";
             this.ButtonBeenden.UseVisualStyleBackColor = true;
@@ -88,7 +88,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(483, 247);
+            this.ClientSize = new System.Drawing.Size(552, 353);
             this.Controls.Add(this.ButtonBeenden);
             this.Controls.Add(this.listBox3);
             this.Controls.Add(this.listBox2);
