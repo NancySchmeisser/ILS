10b1ced00f6c446f7a213022ecfeef659e11105b
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-11-19 09:31:25 +0100

    Einsendaufgabe 3

---
+++ CSHP12D/Einsendeaufgabe/CSHP12D Einsendeaufgabe 3/CSHP12D Einsendeaufgabe 3/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace CSHP12D_Einsendeaufgabe_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSpeichern_Click(object sender, EventArgs e)
+        {
+            var directory =  Path.GetDirectoryName(Application.ExecutablePath);
+            var fileName = Path.Combine(directory, "liste.xml");
+            XmlWriter xmlSpeichern = XmlWriter.Create(fileName);
+            xmlSpeichern.WriteStartDocument();
+            xmlSpeichern.WriteStartElement("liste");
+            for(int i = 0; i< listBox1.Items.Count; i++)
+            {
+                xmlSpeichern.WriteElementString("Wert" + (i + 1), listBox1.Items[i].ToString());
+            }
+            xmlSpeichern.WriteEndElement();
+            xmlSpeichern.WriteEndDocument();
+            xmlSpeichern.Close();
+        }
+
+        private void ButtonUebernehmen_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+        }
+    }
+}
