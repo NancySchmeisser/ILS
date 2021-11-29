814648976dfc4919027633f87cd641b9e48ad987
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-11-29 08:59:45 +0100

    add code 2.1

--- CSHP13D/CSHP13D 2.2/CSHP13D 2.2/Form1.cs
+++ CSHP13D/CSHP13D 2.2/CSHP13D 2.2/Form1.cs
@@ -12,9 +12,25 @@ namespace CSHP13D_2._2
 {
     public partial class Form1 : Form
     {
+        int zaehler;
+        string dateiname;
         public Form1()
         {
             InitializeComponent();
+            zaehler = 1;
+        }
+
+        void NeuesDokument()
+        {
+            //den Text löschen
+            richTextBox1.Clear();
+            //die Anzeige in der Titelliste setzen
+            this.Text = "ohnename" + zaehler;
+            //den Zähler um 1 erhöhen
+            zaehler++;
+            //der Dateiname ist leer
+            dateiname = string.Empty;
+
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
