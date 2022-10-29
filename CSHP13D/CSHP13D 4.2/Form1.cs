a2dd81aedc2de37babe22423cec4f0e5c9219b75
NancySchmeisser <nancy.schmeisser@netzalist.de>
2022-10-29 21:24:44 +0200

    add code 4.2

---
+++ CSHP13D/CSHP13D 4.2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSHP13D_4._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSuche_Click(object sender, EventArgs e)
+        {
+            string dateiname;
+            int treffer = 0;
+            int punkte = 0;
+            int neuePosition = 0;
+            string name;
+            //die Liste leeren
+            listBox1.Items.Clear();
+            //ist das Suchkriterium leer?
+            if (textBox1.Text == string.Empty)
+            {
+
+            }
+        }
+    }
+}
